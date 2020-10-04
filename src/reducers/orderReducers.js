3531d0d39b27759d40d863278c3fa5f61b78e333
Aditi-Chowdhuri <aditinirapada.2018@vitstudent.ac.in>
2020-10-05 01:22:54 +0530

    react-shopping-cart

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -1,19 +1,3 @@
-/*import { CREATE_ORDER, CLEAR_ORDER, FETCH_ORDERS } from "../types";
-
-const orderReducer = (state = {}, action) => {
-  switch (action.type) {
-    case CREATE_ORDER:
-      return { order: action.payload };
-    case CLEAR_ORDER:
-      return { order: null };
-    case FETCH_ORDERS:
-      return { orders: action.payload };
-    default:
-      return state;
-  }
-};
-export { orderReducer };
-*/
 import { CREATE_ORDER, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 
 const orderReducer = (state = {}, action) => {
